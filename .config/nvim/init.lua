ebb52eefabaa01f33d58d22ec8acc7fc65dde6c9
Laker Turner <la@laker.gay>
2024-04-13 12:29:03 +0100

    pretend this is part of the last commit <3

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -718,7 +718,7 @@ require('lazy').setup {
   --require 'kickstart.plugins.debug',
   --require 'kickstart.plugins.indent_line',
 
-  { import = 'custom.plugins' },
+  { import = 'plugins' },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
