de0f7694c7a659d1d6495aef1cd890b618a8eed7
Laker Turner <la@laker.gay>
2024-06-08 08:50:16 +0100

    barbecue fiddling

--- .config/nvim/lua/plugins/barbecue.lua
+++ .config/nvim/lua/plugins/barbecue.lua
@@ -1,18 +1,15 @@
 return {
-  'utilyre/barbecue.nvim',
-  name = 'barbecue',
-  version = '*',
-  dependencies = {
-    'SmiteshP/nvim-navic',
-    'nvim-tree/nvim-web-devicons', -- optional dependency
-  },
-  opts = {
-    -- configurations go here
-  },
-  config = function()
-    require('barbecue').setup {
-      theme = 'catppuccin',
-    }
-    vim.keymap.set('n', '<leader>vb', '<cmd>lua require("barbecue.ui").toggle()<CR>', { desc = 'Toggle barbecue' })
-  end,
+    'utilyre/barbecue.nvim',
+    name = 'barbecue',
+    version = '*',
+    dependencies = {
+        'SmiteshP/nvim-navic',
+        'nvim-tree/nvim-web-devicons', -- optional dependency
+    },
+    config = function()
+        require('barbecue').setup {
+            theme = 'catppuccin',
+        }
+        vim.keymap.set('n', '<leader>vb', '<cmd>lua require("barbecue.ui").toggle()<CR>', { desc = 'Toggle barbecue' })
+    end,
 }
